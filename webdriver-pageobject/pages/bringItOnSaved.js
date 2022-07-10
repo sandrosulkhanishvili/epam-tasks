c508eeafdde99ec17933300c998ba021f0f48e74
Sandro Sulkhanishvili <sandro.sulkhanishvili@gmail.com>
2022-07-11 00:59:36 +0400

    removed previous commit

--- webdriver-pageobject/pages/bringItOnSaved.js
+++
@@ -1,8 +0,0 @@
-//tried adding seccodn page where all the testing should take place but didn't work
-const { By } = require("selenium-webdriver");
-
-class BringItOnSaved {
-  constructor() {}
-}
-
-module.exports = BringItOnSaved;
--- webdriver-pageobject/test/config/.mocharc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  diff: true,
-  reporter: "spec",
-  spec: "test/specs/**.spec.js",
-  timeout: 60000,
-};
